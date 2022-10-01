a671f77eace3274583d2538e0172c8af0a0dd911
Muni Sneha Sai Chembeti <91887869+SnehaChembeti@users.noreply.github.com>
2022-10-01 22:43:40 +0530

    Update Arithmetic and Variables.py

--- Arithmetic and Variables.py
+++ Arithmetic and Variables.py
@@ -1,1 +1,44 @@
+# PRINTING
+print("Hello World!")
 
+
+# COMMENTING - to say what actually the piece of code is doing
+# symbol - (#) 
+
+
+# ARITHMETIC
+print(2+1)
+print(9-5)
+# Addition (+), Substraction(-), Multiplication(*), Division(/), Exponent(**)
+print(((1 + 3) * (9 - 2) / 2) ** 2) #Follows pedmas rule (dm - left to right and as - left to right)
+
+
+# VARIABLES
+test_var = 4 + 5
+print(test_var)
+#can't have spaces
+#can only include letters, numbers and underscores
+#have to start with a letter or underscore
+#Then, to create the variable, you need to use = to assign the value that you want it to have.
+
+
+# MANIPULATING VARIABLES
+# You can always change the value assigned to a variable by overriding the previous value.
+my_var = 3
+print(my_var)
+my_var = 100
+print(my_var)
+
+
+# USING MULTIPLE VARIABLES
+num_years = 4
+days_per_year = 365 
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
+
+
+# DEBUGGING
+# One common error when working with variables is to accidentally introduce typos - NameError
